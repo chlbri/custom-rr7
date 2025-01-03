348107b792c42bff908bf1c90b6107e1236cfdbc
chlbri <bri_lvi@icloud.com>
2025-01-03 12:10:51 +0000

    ok

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,3 @@ import { base, listener } from './index.helpers.js';
 const server = createServer(listener).listen(PORT, () => {
   console.log(`Listening on ${base}`);
 });
-
-export default server;
