508ec0003ade2ae153410c593ed71a864b289f95
chlbri <bri_lvi@icloud.com>
2025-01-03 11:50:15 +0000

    ok rewrites #12

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { createServer } from 'http';
 import { PORT } from './env.js';
 import { base, listener } from './index.helpers.js';
 
-createServer(listener).listen(PORT, () => {
+const server = createServer(listener).listen(PORT, () => {
   console.log(`Listening on ${base}`);
 });
+
+export default server;
